0c999397857e43f1a3dbdefacdeafd6e7dfff606
Prathamesh18X <prathmeshpatil418@gmail.com>
2025-01-04 14:44:35 +0530

    [FEATURE]:Created folder structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>
+          Hi.. there We are here to Prepare
+          <span style={{ color: "green" }}> kloser </span>
+        </h1>
+
+        <h4>(welcome to the project)</h4>
+      </div>
+    </>
+  );
+}
+
+export default App;
